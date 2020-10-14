e4b47aa6f139cd92e231b2615c444ab807c428f9
Sergey <mcheguevara2@gmail.com>
2020-10-15 01:04:07 +0300

    fix

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Col, Container, Row } from "react-bootstrap"
+
+export default function Home() {
+  return (
+    <div className="app">
+      <Container>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
