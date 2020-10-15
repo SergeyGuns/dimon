df4806956ac67ea561e256ef2461ddb650574893
Sergey <mcheguevara2@gmail.com>
2020-10-15 03:05:11 +0300

    fix

--- gatsby-site/src/pages/index.jsx
+++ gatsby-site/src/pages/index.jsx
@@ -72,6 +72,53 @@ export default function Home() {
           </Col>
         </Row>
       </Container>
+      <Container>
+        <Row>
+          <Col style={{ position: "relative" }}>
+            <div
+              style={{
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "white",
+              }}
+            >
+              <div data-aos="fade-up" className="img__text">
+                <h2 style={{ color: "black" }}>
+                  HELLO.
+                  <br />
+                  MY NAME
+                  <br />
+                  IS VASYA.
+                  <br />
+                  I'M AN ART DIRECTOR.
+                  <br />
+                  HERE ARE SOME OF
+                  <br />
+                  MY WORKS:
+                </h2>
+
+                <span></span>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col style={{ position: "relative" }}>
+            <img
+              className="layout-img"
+              alt="girl"
+              src="https://sergeyguns.github.io/dimon/pairs/01_1.jpg"
+            />
+            <div data-aos="fade-up" className="img__text">
+              <h2 style={{ color: "white" }}>HELLO.</h2>
+
+              <span style={{ color: "white" }}> Fish text.</span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
       {menuIsOpen && (
         <Menu>
           <div>1. #########</div>
