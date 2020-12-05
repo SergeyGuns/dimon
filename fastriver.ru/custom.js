4d6b939aea3bc1894f0e870db925f92890d10c78
Sergey <mcheguevara2@gmail.com>
2020-12-05 09:12:11 +0300

    fix

--- fastriver.ru/custom.js
+++ fastriver.ru/custom.js
@@ -11,7 +11,7 @@ var menuColor = MENU_COLOR.blue;
 
 var scrollPrev = 0;
 var isOut = false;
-console.warn("v0.0.4");
+console.warn("v0.0.5");
 
 function getColorByScroll(scrolled) {
   var scrolled = window.scrollY;
